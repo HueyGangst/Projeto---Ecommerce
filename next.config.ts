4ade8e899ff3c314e1abbf8b565b4ffbf717e18f
HueyGangst <120142438+HueyGangst@users.noreply.github.com>
2025-09-01 18:39:32 -0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  // Habilita warnings de hidratação em desenvolvimento
+  reactStrictMode: true,
+};
+
+export default nextConfig;
